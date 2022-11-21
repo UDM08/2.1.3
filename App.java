13fc9ae6a27c2714a86bce3c814df1c161a7dbea
UDM08 <damir_usmanov92@list.ru>
2022-11-21 11:46:58 +0300

    1

---
+++ App.java
@@ -0,0 +1,24 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1.getMessage());
+        Cat one =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(one.getMessage());
+        Cat two =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(two.getMessage());
+
+        System.out.println(bean.equals(bean1));
+        System.out.println(one.equals(two));
+    }
+}
